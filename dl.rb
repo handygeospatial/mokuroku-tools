737e6c42143015b7b97198a562b210a80d6ba22e
handygeospatial <fujimura.hidenori@gmail.com>
2015-01-31 06:54:18 +0900

    Reduced the amount of output to $stdout.

--- dl.rb
+++ dl.rb
@@ -3,32 +3,34 @@ require 'digest/md5'
 require 'fileutils'
 require 'zlib'
 
+$simple = true
+
 Zlib::GzipReader.open('mokuroku.csv.gz').each_line {|l|
-  next unless rand(1) == 0
+  #next unless rand(1) == 0
   (path, date, size, md5) = l.strip.split(',')
   url = "http://cyberjapandata.gsi.go.jp/xyz/std/#{path}"
-  next unless (8..15).include?(path.split('/')[0].to_i)
+  #next unless (18..18).include?(path.split('/')[0].to_i)
   if File.exist?("#{path}") && Digest::MD5.file(path) == md5
-    print "skipped #{url}.\n"
+    print $simple ? '-' : "skipped #{url}.\n"
     next
   end
   buf = open(url).read
   buf_md5 = Digest::MD5.hexdigest(buf)
   if md5 != buf_md5
-    print <<-EOS
+    print $simple ? 'x' : <<-EOS
 different MD5: #{url}
   #{md5}, #{size}B for mokuroku
   #{buf_md5}, #{buf.size}B from the web
     EOS
     if File.exist?(path)
       FileUtils.rm(path)
-      print "deleted #{path}.\n"
+      print $simple ? 'k' : "deleted #{path}.\n"
     end
   end
   [File.dirname(path)].each{|it|
     FileUtils.mkdir_p(it) unless File.directory?(it)
   }
   File.open("#{path}", 'w') {|w| w.print buf}
-  print "Downloaded #{path}#{'.' * (buf.size / 1000)}\n"
+  print $simple ? 'o' : "Downloaded #{path}#{'.' * (buf.size / 1000)}\n"
   #sleep rand(3)
 }
